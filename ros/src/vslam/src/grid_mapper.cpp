c875630fbca982e881b35fe367bf958341e515df
Sebastian Theiler <25444757+sebtheiler@users.noreply.github.com>
2024-02-22 16:25:16 -0600

    Refactored new grid mapping code

--- ros/src/vslam/src/grid_mapper.cpp
+++ ros/src/vslam/src/grid_mapper.cpp
@@ -48,43 +48,6 @@ vector<Point> bresenham(int x1, int y1, int x2, int y2) {
   return points;
 }
 
-Point xzToGrid(float x, float z, float MAX_XZ, int grid_size) {
-  int grid_x = (x + MAX_XZ) / (2*MAX_XZ) * grid_size;
-  int grid_z = (z + MAX_XZ) / (2*MAX_XZ) * grid_size;
-  grid_x = min(max(grid_x, 0), grid_size - 1);
-  grid_z = min(max(grid_z, 0), grid_size - 1);
-  return {grid_x, grid_z};
-}
-
-void display_grid_map(vector<vector<GridCell>> &grid_map, cv::Mat img, vector<float> pose) {
-  int MAX_XZ = 10; // made up number
-  int DISPLAY_GRID_MIN = 3; // made up number (number of points a cell needs to be visible at all)
-
-  img.setTo(cv::Scalar(255, 255, 255));
-  int img_size = img.size().width;
-  int pixels_per_cell = img_size / grid_map.size();
-
-  int grid_size = grid_map.size();
-  Point pose_grid_point = xzToGrid(pose[0], pose[2], MAX_XZ, grid_size);
-
-  for (int i = 0; i < grid_size; i++) {
-    for (int j = 0; j < grid_size; j++) {
-      cv::Scalar color;
-      if (i == pose_grid_point.x && j == pose_grid_point.y) {
-        color = cv::Scalar(0, 0, 255);
-      } else if (grid_map[i][j].nVisited < DISPLAY_GRID_MIN) {
-        color = cv::Scalar(255, 120, 120);
-      } else {
-        GridCell cell = grid_map[i][j];
-        int color_value = (1 - ((float)cell.nOccupied / (cell.nVisited + 1))) * 255;
-        cout << "nOccupied: " << cell.nOccupied << " nVisited: " << cell.nVisited << " color_value: " << color_value << endl;
-        color = cv::Scalar(color_value, color_value, color_value);
-      }
-      cv::rectangle(img, cv::Rect(i*pixels_per_cell, j*pixels_per_cell, pixels_per_cell, pixels_per_cell), color, cv::FILLED);
-    }
-  }
-}
-
 class GridMapperNode : public rclcpp::Node {
   private:
     rclcpp::Subscription<vslam::msg::PointCloudAndPose>::SharedPtr point_cloud_and_pose_sub_;
@@ -93,61 +56,161 @@ class GridMapperNode : public rclcpp::Node {
     cv::Mat img;
     vector<vector<GridCell>> grid_map;
 
+    int n_prev_keyframes = 0;
+
+    int GRID_SIZE_CM;
+    int GRID_RESOLUTION_CM;
+    int PIXELS_PER_CELL;
+    int MAX_XZ;
+    int DISPLAY_GRID_MIN;
+
+    int IMG_SIZE;
+    int GRID_SIZE;
+
+    Point xzToGrid(float x, float z) {
+      int grid_x = (x + MAX_XZ) / (2*MAX_XZ) * GRID_SIZE;
+      int grid_z = (z + MAX_XZ) / (2*MAX_XZ) * GRID_SIZE;
+      grid_x = min(max(grid_x, 0), GRID_SIZE - 1);
+      grid_z = min(max(grid_z, 0), GRID_SIZE - 1);
+      return {grid_x, grid_z};
+    }
+
+    void update_grid_map(vector<vslam::msg::MapPoint> map_points, vector<vslam::msg::KeyFrame> key_frames) {
+      // Create a map for quick lookup of map points by their IDs
+      unordered_map<int, vslam::msg::MapPoint> map_points_map;
+      for (const auto& map_point : map_points) {
+        map_points_map[map_point.id] = map_point;
+      }
+
+      for (const auto& key_frame : key_frames) {
+        // Calculate grid point for key frame once
+        Point key_frame_grid_point = xzToGrid(key_frame.pose.position.x, key_frame.pose.position.z);
+
+        // Update grid map by drawing lines between key frame and its map points
+        for (auto map_point_id : key_frame.map_point_ids) {
+          auto it = map_points_map.find(map_point_id);
+          if (it != map_points_map.end()) {
+            const auto& map_point = it->second;
+            // Calculate grid point for map point once
+            Point map_point_grid_point = xzToGrid(map_point.world_pos.x, map_point.world_pos.z);
+
+            // Check if points are outside the grid map
+            if (key_frame_grid_point.x >= GRID_SIZE || key_frame_grid_point.y >= GRID_SIZE || 
+                map_point_grid_point.x >= GRID_SIZE || map_point_grid_point.y >= GRID_SIZE) {
+              continue;
+            }
+
+            // Calculate Bresenham line only for points inside the grid map
+            vector<Point> visited_points = bresenham(key_frame_grid_point.x, key_frame_grid_point.y, map_point_grid_point.x, map_point_grid_point.y);
+            for (const auto& point : visited_points) {
+              grid_map[point.x][point.y].nVisited++;
+            }
+            grid_map[map_point_grid_point.x][map_point_grid_point.y].nOccupied++;
+          }
+        }
+      }
+    }
+
+    void display_grid_map(geometry_msgs::msg::Pose pose) {
+      img.setTo(cv::Scalar(255, 255, 255));
+      int grid_size = grid_map.size();
+      Point pose_grid_point = xzToGrid(pose.position.x, pose.position.z);
+
+      for (int i = 0; i < grid_size; i++) {
+        for (int j = 0; j < grid_size; j++) {
+          cv::Scalar color;
+
+          // Check if the current cell corresponds to the pose
+          if (i == pose_grid_point.x && j == pose_grid_point.y) {
+            color = cv::Scalar(0, 0, 255);
+          } else {
+            GridCell cell = grid_map[i][j];
+            // Calculate color value based on occupancy
+            int color_value = (1 - ((float)cell.nOccupied / (cell.nVisited + 1))) * 255;
+
+            // Adjust color if below minimum display threshold
+            if (cell.nVisited < DISPLAY_GRID_MIN) {
+              color = cv::Scalar(255, 120, 120);
+            } else {
+              color = cv::Scalar(color_value, color_value, color_value);
+            }
+          }
+
+          // Draw rectangle with appropriate color
+          cv::rectangle(img, cv::Rect(i * PIXELS_PER_CELL, j * PIXELS_PER_CELL, PIXELS_PER_CELL, PIXELS_PER_CELL), color, cv::FILLED);
+        }
+      }
+    }
+
     void map_callback(const vslam::msg::Map::SharedPtr msg)
     {
       vector<vslam::msg::MapPoint> map_points = msg->map_points;
       vector<vslam::msg::KeyFrame> key_frames = msg->key_frames;
       geometry_msgs::msg::Pose pose = msg->camera_pose;
-      for (auto key_frame : key_frames) {
-        // Get corresponding map points for this key frame
-        vector<vslam::msg::MapPoint> key_frame_map_points;
-        for (auto map_point_id : key_frame.map_point_ids) {
-          for (auto map_point : map_points) {
-            if (map_point.id == map_point_id) {
-              key_frame_map_points.push_back(map_point);
-              break;
-            }
-          }
-        }
-
-        // Update grid map by drawing lines between key frame and its map points
-        Point key_frame_grid_point = xzToGrid(key_frame.pose.position.x, key_frame.pose.position.z, 10, grid_map.size());
-        for (auto map_point : key_frame_map_points) {
-          Point map_point_grid_point = xzToGrid(map_point.world_pos.x, map_point.world_pos.z, 10, grid_map.size());
-          // Just ignore points that are outside the grid map
-          if (key_frame_grid_point.x > grid_map.size() || key_frame_grid_point.y > grid_map.size() || map_point_grid_point.x > grid_map.size() || map_point_grid_point.y > grid_map.size()) {
-            continue;
-          }
-          vector<Point> visited_points = bresenham(key_frame_grid_point.x, key_frame_grid_point.y, map_point_grid_point.x, map_point_grid_point.y);
-          for (auto point : visited_points) {
-            grid_map[point.x][point.y].nVisited++;
-          }
-          grid_map[map_point_grid_point.x][map_point_grid_point.y].nOccupied++;
-        }
+      
+      int n_keyframes = key_frames.size();
+      if (n_keyframes == n_prev_keyframes) {
+        return;
       }
+      n_prev_keyframes = n_keyframes;
 
-      // Display grid map
-      vector<float> vPose = {pose.position.x, pose.position.y, pose.position.z, pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w};
-      display_grid_map(grid_map, img, vPose);
-      cv::imshow("Grid", img);
-      cv::waitKey(1);
+      update_grid_map(map_points, key_frames);
+
+      if (this->get_parameter("display_grid").as_bool()) {
+        display_grid_map(pose);
+        cv::imshow("Grid", img);
+        cv::waitKey(1);
+      }
     }
 
   public:
     GridMapperNode() : Node("grid_mapper")
   {
-    map_sub_ = this->create_subscription<vslam::msg::Map>("vslam/map", 10, bind(&GridMapperNode::map_callback, this, placeholders::_1));
+    auto display_grid_param_desc = rcl_interfaces::msg::ParameterDescriptor{};
+    display_grid_param_desc.description = "Display grid map?";
+    this->declare_parameter<bool>("display_grid", true, display_grid_param_desc);
 
-    int GRID_SIZE_CM = 3*100;
-    int GRID_RESOLUTION_CM = 1;
-    int PIXELS_PER_CELL = 4;
+    auto map_topic_param_desc = rcl_interfaces::msg::ParameterDescriptor{};
+    map_topic_param_desc.description = "Name of the topic to subscribe to for the map";
+    this->declare_parameter<string>("map_topic", "vslam/map", map_topic_param_desc);
 
-    int img_size = GRID_SIZE_CM / GRID_RESOLUTION_CM * PIXELS_PER_CELL;
-    img = cv::Mat::zeros(img_size, img_size, CV_8UC3);
+    auto grid_size_cm_param_desc = rcl_interfaces::msg::ParameterDescriptor{};
+    grid_size_cm_param_desc.description = "Size of the grid in centimeters";
+    this->declare_parameter<int>("grid_size_cm", 300, grid_size_cm_param_desc);
 
-    grid_map.resize(GRID_SIZE_CM / GRID_RESOLUTION_CM, vector<GridCell>(GRID_SIZE_CM / GRID_RESOLUTION_CM, {0, 0}));
-    for (size_t i = 0; i < grid_map.size(); i++) {
-      for (size_t j = 0; j < grid_map[i].size(); j++) {
+    auto grid_resolution_cm_param_desc = rcl_interfaces::msg::ParameterDescriptor{};
+    grid_resolution_cm_param_desc.description = "Resolution of the grid in centimeters";
+    this->declare_parameter<int>("grid_resolution_cm", 1, grid_resolution_cm_param_desc);
+
+    auto pixels_per_cell_param_desc = rcl_interfaces::msg::ParameterDescriptor{};
+    pixels_per_cell_param_desc.description = "Number of pixels per cell for image display";
+    this->declare_parameter<int>("pixels_per_cell", 4, pixels_per_cell_param_desc);
+
+    auto max_xz_param_desc = rcl_interfaces::msg::ParameterDescriptor{};
+    max_xz_param_desc.description = "Maximum value of x and z in the world";
+    this->declare_parameter<int>("max_xz", 10, max_xz_param_desc);
+
+    auto display_grid_min_param_desc = rcl_interfaces::msg::ParameterDescriptor{};
+    display_grid_min_param_desc.description = "Minimum number of points a cell needs to be visible at all";
+    this->declare_parameter<int>("display_grid_min", 3, display_grid_min_param_desc);
+
+    GRID_SIZE_CM = this->get_parameter("grid_size_cm").as_int();
+    GRID_RESOLUTION_CM = this->get_parameter("grid_resolution_cm").as_int();
+    PIXELS_PER_CELL = this->get_parameter("pixels_per_cell").as_int();
+    MAX_XZ = this->get_parameter("max_xz").as_int();
+    DISPLAY_GRID_MIN = this->get_parameter("display_grid_min").as_int();
+    IMG_SIZE = GRID_SIZE_CM / GRID_RESOLUTION_CM * PIXELS_PER_CELL;
+    GRID_SIZE = GRID_SIZE_CM / GRID_RESOLUTION_CM;
+
+    // Subscribe to point cloud and pose
+    string map_topic = this->get_parameter("map_topic").as_string();
+    map_sub_ = this->create_subscription<vslam::msg::Map>(map_topic, 10, bind(&GridMapperNode::map_callback, this, placeholders::_1));
+    img = cv::Mat::zeros(IMG_SIZE, IMG_SIZE, CV_8UC3);
+
+    // Initialize grid map
+    grid_map.resize(GRID_SIZE, vector<GridCell>(GRID_SIZE, {0, 0}));
+    for (int i = 0; i < GRID_SIZE; i++) {
+      for (int j = 0; j < GRID_SIZE; j++) {
         grid_map[i][j] = {0, 0};
       }
     }
--- ros/src/vslam/src/system_node.cpp
+++ ros/src/vslam/src/system_node.cpp
@@ -6,11 +6,7 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/image_encodings.hpp"
-#include "sensor_msgs/msg/point_cloud2.hpp"
-#include "sensor_msgs/point_cloud2_iterator.hpp"
-#include "geometry_msgs/msg/pose_stamped.hpp"
 
-#include "vslam/msg/point_cloud_and_pose.hpp"
 #include "vslam/msg/map.hpp"
 #include "vslam/msg/key_frame.hpp"
 #include "vslam/msg/map_point.hpp"
@@ -26,10 +22,11 @@
 using namespace std;
 
 void throttleCallback(const chrono::time_point<chrono::high_resolution_clock>& last_callback) {
-  // auto now = chrono::high_resolution_clock::now();
-  // if (now - last_callback < chrono::milliseconds(1000 / 10)) {
-  //   return;
-  // }
+  int FPS = 10;
+  auto now = chrono::high_resolution_clock::now();
+  if (now - last_callback < chrono::milliseconds(1000 / FPS)) {
+    return;
+  }
   return;
 }
 
@@ -45,72 +42,7 @@ cv_bridge::CvImagePtr getImageFromMessage(const sensor_msgs::msg::Image::ConstSh
 }
 
 void processImage(cv::Mat& image) {
-  // cv::resize(image, image, cv::Size(752, 480));
   cv::resize(image, image, cv::Size(752, 480));
-}
-
-vslam::msg::PointCloudAndPose::SharedPtr createPointCloudAndPoseMessage(
-  const vector<ORB_SLAM3::MapPoint*>& map_points,
-  const Sophus::SE3f& pose,
-  rclcpp::Time& current_time
-) {
-  vslam::msg::PointCloudAndPose::SharedPtr point_cloud_and_pose_msg = std::make_shared<vslam::msg::PointCloudAndPose>();
-
-  Eigen::Vector3f position = pose.translation();
-  Eigen::Quaternionf orientation = pose.unit_quaternion();
-
-  point_cloud_and_pose_msg->pose.position.x = position.x();
-  point_cloud_and_pose_msg->pose.position.y = position.y();
-  point_cloud_and_pose_msg->pose.position.z = position.z();
-  point_cloud_and_pose_msg->pose.orientation.x = orientation.x();
-  point_cloud_and_pose_msg->pose.orientation.y = orientation.y();
-  point_cloud_and_pose_msg->pose.orientation.z = orientation.z();
-  point_cloud_and_pose_msg->pose.orientation.w = orientation.w();
-
-  sensor_msgs::PointCloud2Modifier modifier(point_cloud_and_pose_msg->pointcloud);
-  modifier.setPointCloud2Fields(
-    3,
-    "x", 1, sensor_msgs::msg::PointField::FLOAT32,
-    "y", 1, sensor_msgs::msg::PointField::FLOAT32,
-    "z", 1, sensor_msgs::msg::PointField::FLOAT32
-  );
-
-  vector<int> indicies_to_publish;
-  for (unsigned int i = 0; i < map_points.size(); i++) {
-    auto mp = map_points[i];
-    if (mp == nullptr || mp->isBad()) {
-      continue;
-    }
-
-    indicies_to_publish.push_back(i);
-  }
-
-  int num_points_to_publish = indicies_to_publish.size();
-  point_cloud_and_pose_msg->pointcloud.header.frame_id = "map";
-  point_cloud_and_pose_msg->pointcloud.header.stamp = current_time;
-  point_cloud_and_pose_msg->pointcloud.height = 1;
-  point_cloud_and_pose_msg->pointcloud.width = num_points_to_publish;
-  point_cloud_and_pose_msg->pointcloud.is_bigendian = false;
-  point_cloud_and_pose_msg->pointcloud.is_dense = true;
-
-  modifier.resize(num_points_to_publish);
-  sensor_msgs::PointCloud2Iterator<float> iter_x(point_cloud_and_pose_msg->pointcloud, "x");
-  sensor_msgs::PointCloud2Iterator<float> iter_y(point_cloud_and_pose_msg->pointcloud, "y");
-  sensor_msgs::PointCloud2Iterator<float> iter_z(point_cloud_and_pose_msg->pointcloud, "z");
-
-  for (int index : indicies_to_publish) {
-    auto mp = map_points[index];
-    Eigen::Vector3f world_pos = mp->GetWorldPos();
-    *iter_x = world_pos.x();
-    *iter_y = world_pos.y();
-    *iter_z = world_pos.z();
-
-    ++iter_x;
-    ++iter_y;
-    ++iter_z;
-  }
-
-  return point_cloud_and_pose_msg;
 }
 
 class VSLAMSystemNode : public rclcpp::Node {
@@ -119,40 +51,50 @@ class VSLAMSystemNode : public rclcpp::Node {
     chrono::time_point<chrono::high_resolution_clock> last_callback;
 
     image_transport::Subscriber image_sub_;
-    rclcpp::Publisher<vslam::msg::PointCloudAndPose>::SharedPtr point_cloud_and_pose_publisher_;
     rclcpp::Publisher<vslam::msg::Map>::SharedPtr map_publisher_;
 
     ORB_SLAM3::System* SLAM;
 
-    void imageCallback(const sensor_msgs::msg::Image::ConstSharedPtr &msg) {
-      throttleCallback(last_callback);
+    void processKeyFrame(ORB_SLAM3::KeyFrame* keyframe, vslam::msg::Map::SharedPtr& map_msg) {
+      Sophus::SE3f keyframe_pose = keyframe->GetPose();
+      Eigen::Vector3f position = keyframe_pose.translation();
+      Eigen::Quaternionf orientation = keyframe_pose.unit_quaternion();
+      vslam::msg::KeyFrame keyframe_msg;
+      keyframe_msg.id = keyframe->mnId;
+      keyframe_msg.pose.position.x = position.x();
+      keyframe_msg.pose.position.y = position.y();
+      keyframe_msg.pose.position.z = position.z();
+      keyframe_msg.pose.orientation.x = orientation.x();
+      keyframe_msg.pose.orientation.y = orientation.y();
+      keyframe_msg.pose.orientation.z = orientation.z();
+      keyframe_msg.pose.orientation.w = orientation.w();
 
-      cv_bridge::CvImagePtr cv_ptr = getImageFromMessage(msg);
-      if (!cv_ptr) {
-        RCLCPP_ERROR(this->get_logger(), "Failed to convert image message to cv_bridge");
-        return;
+      set<ORB_SLAM3::MapPoint*> keyframe_map_points = keyframe->GetMapPoints();
+      for (ORB_SLAM3::MapPoint* map_point : keyframe_map_points) {
+        keyframe_msg.map_point_ids.push_back(map_point->mnId);
       }
 
-      cv_ptr->image.empty(); // required to prevent segfault
+      map_msg->key_frames.push_back(keyframe_msg);
+    }
 
-      processImage(cv_ptr->image);
+    void processMapPoint(ORB_SLAM3::MapPoint* map_point, vslam::msg::Map::SharedPtr& map_msg) {
+      vslam::msg::MapPoint map_point_msg;
+      Eigen::Vector3f position = map_point->GetWorldPos();
+      map_point_msg.id = map_point->mnId;
+      map_point_msg.world_pos.x = position.x();
+      map_point_msg.world_pos.y = position.y();
+      map_point_msg.world_pos.z = position.z();
+      map_msg->map_points.push_back(map_point_msg);
+    }
 
-      double tframe = msg->header.stamp.sec + msg->header.stamp.nanosec / 1000000000.0;
-      Sophus::SE3f pose = SLAM->TrackMonocular(cv_ptr->image, tframe);
-      vector<ORB_SLAM3::MapPoint*> map_points = SLAM->GetTrackedMapPoints();
-
-      rclcpp::Time current_time = this->now();
-
-      vslam::msg::PointCloudAndPose::SharedPtr point_cloud_and_pose_msg = createPointCloudAndPoseMessage(map_points, pose, current_time);
-      point_cloud_and_pose_publisher_->publish(*point_cloud_and_pose_msg);
-
+    void publishMap(const Sophus::SE3f& pose) {
       ORB_SLAM3::Atlas* atlas = SLAM->GetAtlas();
       ORB_SLAM3::Map* map = atlas->GetCurrentMap();
       vector<ORB_SLAM3::KeyFrame*> keyframes = map->GetAllKeyFrames();
       vector<ORB_SLAM3::MapPoint*> all_map_points = map->GetAllMapPoints();
 
       vslam::msg::Map::SharedPtr map_msg = std::make_shared<vslam::msg::Map>();
-      map_msg->header.stamp = current_time;
+      map_msg->header.stamp = this->now();
       map_msg->header.frame_id = "map";
       map_msg->id = map->GetId();
 
@@ -167,38 +109,33 @@ class VSLAMSystemNode : public rclcpp::Node {
       map_msg->camera_pose.orientation.w = camera_orientation.w();
 
       for (ORB_SLAM3::KeyFrame* keyframe : keyframes) {
-        Sophus::SE3f keyframe_pose = keyframe->GetPose();
-        Eigen::Vector3f position = keyframe_pose.translation();
-        Eigen::Quaternionf orientation = keyframe_pose.unit_quaternion();
-        vslam::msg::KeyFrame keyframe_msg;
-        keyframe_msg.id = keyframe->mnId;
-        keyframe_msg.pose.position.x = position.x();
-        keyframe_msg.pose.position.y = position.y();
-        keyframe_msg.pose.position.z = position.z();
-        keyframe_msg.pose.orientation.x = orientation.x();
-        keyframe_msg.pose.orientation.y = orientation.y();
-        keyframe_msg.pose.orientation.z = orientation.z();
-        keyframe_msg.pose.orientation.w = orientation.w();
-
-        set<ORB_SLAM3::MapPoint*> keyframe_map_points = keyframe->GetMapPoints();
-        for (ORB_SLAM3::MapPoint* map_point : keyframe_map_points) {
-          keyframe_msg.map_point_ids.push_back(map_point->mnId);
-        }
-
-        map_msg->key_frames.push_back(keyframe_msg);
+        processKeyFrame(keyframe, map_msg);
       }
 
       for (ORB_SLAM3::MapPoint* map_point : all_map_points) {
-        vslam::msg::MapPoint map_point_msg;
-        Eigen::Vector3f position = map_point->GetWorldPos();
-        map_point_msg.id = map_point->mnId;
-        map_point_msg.world_pos.x = position.x();
-        map_point_msg.world_pos.y = position.y();
-        map_point_msg.world_pos.z = position.z();
-        map_msg->map_points.push_back(map_point_msg);
+        processMapPoint(map_point, map_msg);
       }
 
       map_publisher_->publish(*map_msg);
+    }
+
+    void imageCallback(const sensor_msgs::msg::Image::ConstSharedPtr& msg) {
+      throttleCallback(last_callback);
+
+      cv_bridge::CvImagePtr cv_ptr = getImageFromMessage(msg);
+      if (!cv_ptr) {
+        RCLCPP_ERROR(this->get_logger(), "Failed to convert image message to cv_bridge");
+        return;
+      }
+
+      cv_ptr->image.empty(); // required to prevent segfault
+
+      processImage(cv_ptr->image);
+
+      double tframe = msg->header.stamp.sec + msg->header.stamp.nanosec / 1000000000.0;
+      Sophus::SE3f pose = SLAM->TrackMonocular(cv_ptr->image, tframe);
+
+      publishMap(pose);
 
       cv::imshow(OPENCV_WINDOW, cv_ptr->image);
       cv::waitKey(3);
@@ -234,7 +171,6 @@ class VSLAMSystemNode : public rclcpp::Node {
         custom_qos
     );
 
-    point_cloud_and_pose_publisher_ = this->create_publisher<vslam::msg::PointCloudAndPose>("vslam/point_cloud_and_pose", 10);
     map_publisher_ = this->create_publisher<vslam::msg::Map>("vslam/map", 10);
 
     string vocab_path = this->get_parameter("vocab_path").as_string();
