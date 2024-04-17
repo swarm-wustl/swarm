747ca6a1cbe7d6b6a27546607530757e4f7cb5b0
Julian Clark <julian.clark4455@gmail.com>
2024-04-17 18:06:45 -0500

    More xbee code

--- xbee/basic_xbee.py
+++ xbee/basic_xbee.py
@@ -1,20 +1,55 @@
 from digi.xbee.devices import DigiMeshDevice
+from digi.xbee.util import utils
 import time
 
 usb1 = "/dev/tty.usbserial-D38JO1QS"
 usb2 = "/dev/tty.usbserial-D38JO1U8"
 
 xbee1 = DigiMeshDevice(usb1, 9600)
-xbee1.set_parameter("AP",  1)
 xbee1.open()
-
 xbee2 = DigiMeshDevice(usb2, 9600)
 xbee2.open()
 
+print("Setting xbee parameters...")
+xbee1.set_parameter("ID", utils.hex_string_to_bytes("2015"))
+xbee1.set_parameter("PL",  utils.hex_string_to_bytes("0"))
+xbee1.set_parameter("DH",  utils.hex_string_to_bytes("0"))
+xbee1.set_parameter("DL",  utils.hex_string_to_bytes("FFFF"))
+xbee1.set_parameter("NI",  bytearray("SENDER", 'utf8'))
+xbee1.set_parameter("RP",  utils.hex_string_to_bytes("5"))
+
+xbee2.set_parameter("ID", utils.hex_string_to_bytes("2015"))
+xbee2.set_parameter("PL",  utils.hex_string_to_bytes("0"))
+xbee2.set_parameter("DH",  utils.hex_string_to_bytes("0"))
+xbee2.set_parameter("DL",  utils.hex_string_to_bytes("FFFF"))
+xbee2.set_parameter("NI",  bytearray("RECEIVER", 'utf8'))
+xbee2.set_parameter("RP",  utils.hex_string_to_bytes("5"))
+print("Finished setting xbee parameters")
+
+# print("Discovering network...")
+# network1 = xbee1.get_network()
+# network1.start_discovery_process()
+# while network1.is_discovery_running():
+#     time.sleep(0.5)
+# print("Finished discovering network")
+
+while True:
+    msg = input("Msg: ")
+    if msg == "exit":
+        break
+    elif len(msg) > 0:
+        xbee1.send_data_broadcast(msg)
+        time.sleep(1)
+        # xbee1.send_data_64(xbee2.get_64bit_addr(), msg)
+        rec_msg = xbee2.read_data()
+        if rec_msg is None:
+            print("No message received!")
+        else:
+            print(rec_msg.data.decode())
+    time.sleep(1)
+
 xbee1.close()
 xbee2.close()
-
-# xbee.set_parameter("NI",  bytearray("Yoda", 'utf8'))
 
 # print(xbee1.get_node_id())
 
