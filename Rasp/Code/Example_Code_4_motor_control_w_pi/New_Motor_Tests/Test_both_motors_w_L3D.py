c71ea9703a0642fe0aeb0b7bccdb0d05d6ddedef
B1WAYN3 <112424397+B1WAYN3@users.noreply.github.com>
2024-04-17 19:17:01 -0500

    lolx6

--- Rasp/Code/Example_Code_4_motor_control_w_pi/New_Motor_Tests/Test_both_motors_w_L3D.py
+++ Rasp/Code/Example_Code_4_motor_control_w_pi/New_Motor_Tests/Test_both_motors_w_L3D.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Apr 6 14:27:39 2024
+Created on Wed Apr 17 18:31:57 2024
+
+@author: Wayne
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  6 14:27:39 2024
+
 @author: Wayne
 """
 
@@ -8,7 +16,6 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)  # Set GPIO numbering mode to BCM
-GPIO.setwarnings(False)  # Disable GPIO warnings
 
 # Motor 1 setup
 Motor1A = 6  # Motor 1, Pin A
@@ -29,74 +36,64 @@ GPIO.setup(Motor2A, GPIO.OUT)
 GPIO.setup(Motor2B, GPIO.OUT)
 GPIO.setup(Motor2EN, GPIO.OUT)
 
-# PWM instances for both motors, set to 70% duty cycle
-pwm1 = GPIO.PWM(Motor1EN, 1000)
-pwm2 = GPIO.PWM(Motor2EN, 1000)
-
-'''
-pwm1.start(70)
-print("Testing motor1 Forward: ")
-GPIO.output(Motor1A, GPIO.HIGH)
-GPIO.output(Motor1B, GPIO.LOW)
-time.sleep(4)
-
-print("Testing motor1 Reverse: ")
-GPIO.output(Motor1A, GPIO.LOW)
-GPIO.output(Motor1B, GPIO.HIGH)
-time.sleep(4)
-pwm1.stop()
-'''
-
-pwm2.start(70)
-print("Testing motor2 Forward: ")
-GPIO.output(Motor2A, GPIO.HIGH)
-GPIO.output(Motor2B, GPIO.LOW)
-time.sleep(4)
-
-print("Testing motor2 Reverse: ")
-GPIO.output(Motor2A, GPIO.LOW)
-GPIO.output(Motor2B, GPIO.HIGH)
-time.sleep(4)
-pwm2.stop()
-
-
-GPIO.cleanup()  # Clean up GPIO to ensure a clean exit
-
-'''
-def forward(motor_number):
-    """Function to set the motor direction to forward and run it at 70% power."""
-    if motor_number == 1:
+def forward(motor):
+    """Function to set the motor direction to forward."""
+    if motor == 1:
         GPIO.output(Motor1A, GPIO.HIGH)
         GPIO.output(Motor1B, GPIO.LOW)
-        print("Motor 1 is running forward at 70% power.")
-    elif motor_number == 2:
+        print("Motor 1 set to forward.")
+    elif motor == 2:
         GPIO.output(Motor2A, GPIO.HIGH)
         GPIO.output(Motor2B, GPIO.LOW)
-        print("Motor 2 is running forward at 70% power.")
+        print("Motor 2 set to forward.")
 
-def reverse(motor_number):
-    """Function to set the motor direction to reverse and run it at 70% power."""
-    if motor_number == 1:
+def reverse(motor):
+    """Function to set the motor direction to reverse."""
+    if motor == 1:
         GPIO.output(Motor1A, GPIO.LOW)
         GPIO.output(Motor1B, GPIO.HIGH)
-        print("Motor 1 is running in reverse at 70% power.")
-    elif motor_number == 2:
+        print("Motor 1 set to reverse.")
+    elif motor == 2:
         GPIO.output(Motor2A, GPIO.LOW)
         GPIO.output(Motor2B, GPIO.HIGH)
-        print("Motor 2 is running in reverse at 70% power.")
+        print("Motor 2 set to reverse.")
 
-# Test each motor in forward and reverse directions
-for motor_id in [1, 2]:
-    # Test forward direction
-    forward(motor_id)
-    time.sleep(5)  # Run each test for 5 seconds
+def ramp_up(pwm, motor_number):
+    """Function to gradually increase the motor's speed."""
+    pwm.start(80)  # Start PWM with 80% duty cycle
+    print(f"Ramping up Motor {motor_number}: Starting at 80% power.")
+    for i in range(80, 110, 10):  # Increase duty cycle from 80% to 100% in steps of 10%
+        pwm.ChangeDutyCycle(i)
+        if i > 100:
+            print(f"Motor {motor_number} at maximum power: 100%.")
+        else:
+            print(f"Motor {motor_number} power level: {i}%.")
+        time.sleep(5)
 
-    # Test reverse direction
-    reverse(motor_id)
-    time.sleep(5)  # Run each test for 5 seconds
+pwm1 = GPIO.PWM(Motor1EN, 1000)  # PWM instance for Motor 1
+pwm2 = GPIO.PWM(Motor2EN, 1000)  # PWM instance for Motor 2
 
-    # Stop the motor
-    (pwm1 if motor_id == 1 else pwm2).stop()
+# Motor 1 forward and ramp up
+# forward(1)
+# ramp_up(pwm1, 1)
+# pwm1.stop()
+
+# Motor 2 forward and ramp up
+forward(2)
+ramp_up(pwm2, 2)
+pwm2.stop()
+
+time.sleep(1)
+
+# Motor 1 reverse and ramp up
+reverse(1)
+ramp_up(pwm1, 1)
+pwm1.stop()
+
+# Motor 2 reverse and ramp up
+reverse(2)
+ramp_up(pwm2, 2)
+pwm2.stop()
 
 GPIO.cleanup()  # Clean up GPIO to ensure a clean exit
-''' 
+
