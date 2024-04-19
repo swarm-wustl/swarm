3736c6f6c4a77d9ebdee4c1b2242a222465f4c5d
Ben Modiano <bmodiano@wustl.edu>
2024-04-19 18:15:26 -0500

    Changes to servo and motor driver python files

---
+++ test-motors/servo_ball.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BOARD)
+import time
+
+
+
+
+# Servo Setup (FOR SWARM PI FOUR!)
+# Define pin, frequency and duty cycle
+servo_pin  = 40
+freq = 50
+
+GPIO.setup(servo_pin, GPIO.OUT)
+servo_pwm = GPIO.PWM(servo_pin, freq) 
+
+servo_pwm.start(7)
+time.sleep(1)
+
+servo_pwm.start(6)
+time.sleep(1)
+
+servo_pwm.start(7)
+time.sleep(1)
