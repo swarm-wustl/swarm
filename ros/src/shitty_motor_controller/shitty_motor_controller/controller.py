b25d08bc1b04f270f85942b0e1ac8cd5d37f277d
Sebastian Theiler <25444757+sebtheiler@users.noreply.github.com>
2024-04-18 21:52:52 -0500

    Added encoder feedback

--- ros/src/shitty_motor_controller/shitty_motor_controller/controller.py
+++ ros/src/shitty_motor_controller/shitty_motor_controller/controller.py
@@ -53,12 +53,7 @@ class Controller(Node):
         # self.publisher = self.create_publisher(Twist, '/left_encoder', 10)
 
         # timer
-<<<<<<< HEAD
         self.timer = self.create_timer(0.01, self.timer_callback)
-=======
-        self.timer = self.create_timer(0.1, self.timer_callback)
-        print('timer')
->>>>>>> d6eb8aa2b0678d2b2f269667d0583389d5de63e4
 
         self.target_left_speed = 0
         self.target_right_speed = 0
@@ -67,16 +62,21 @@ class Controller(Node):
         self.in1A = 36
         self.in2A = 38
         self.enA  = 32
-        self.c1 = 24
-        self.c2 = 26
+        self.c1A = 24
+        self.c2A = 26
 
         # Motor 2
         self.in1B = 35
         self.in2B = 37
         self.enB  = 33
+        self.c1B = 31
+        self.c2B = 29
 
-        GPIO.setup(self.c1, GPIO.IN)
-        GPIO.setup(self.c2, GPIO.IN)
+        GPIO.setup(self.c1A, GPIO.IN)
+        GPIO.setup(self.c2A, GPIO.IN)
+
+        GPIO.setup(self.c1B, GPIO.IN)
+        GPIO.setup(self.c2B, GPIO.IN)
 
         GPIO.setwarnings(False)
 
@@ -86,13 +86,19 @@ class Controller(Node):
         time.sleep(0.25)
         print('init')
 
-        first_val = GPIO.input(self.c1)
-        self.look_for_1 = not first_val
-        self.n = 10
-        self.i = 0
-        self.transitionTimes = [0] * self.n
+        self.look_for_1A = True
+        self.look_for_1B = True
+        self.n = 20
+        self.a = 0
+        self.b = 0
+        self.transitionTimesA = [0] * self.n
+        self.transitionTimesB = [0] * self.n
         self.startTime = time.time()
+        self.currentTime = self.startTime
 
+        self.error = 0
+        self.prev_error = 0
+        self.sum_error = 0
 
     def listener_callback(self, msg):
         x_vel = msg.linear.x
@@ -103,14 +109,8 @@ class Controller(Node):
         right_speed = x_vel + z_ang
 
         # slowly approach target speeds
-<<<<<<< HEAD
         self.target_left_speed = 0.95*self.target_left_speed + 0.05*left_speed
         self.target_right_speed = 0.95*self.target_right_speed + 0.05*right_speed
-=======
-        self.target_left_speed = 0.9*self.target_left_speed + 0.1*left_speed
-        self.target_right_speed = 0.9*self.target_right_speed + 0.1*right_speed
-        print('target', self.target_left_speed, self.target_right_speed)
->>>>>>> d6eb8aa2b0678d2b2f269667d0583389d5de63e4
 
     def timer_callback(self):
         # set motor speeds
@@ -118,7 +118,6 @@ class Controller(Node):
         motor2 = int(abs(self.target_right_speed)*100)
         motor1 = min(90, motor1) if motor1 > 20 else 0
         motor2 = min(90, motor2) if motor2 > 20 else 0
-        print('pwms', motor1, motor2)
 
         motor_pwm_change(self.pwm_pinA, motor1)
         motor_pwm_change(self.pwm_pinB, motor2)
@@ -132,23 +131,45 @@ class Controller(Node):
         self.target_right_speed *= 0.7
 
         # get encoder data
-        c1_val = GPIO.input(self.c1)
-        # c2_val = GPIO.input(self.c2)
+        c1a_val = GPIO.input(self.c1A)
+        c1b_val = GPIO.input(self.c1B)
 
-        if self.look_for_1 and c1_val == 0:
-            self.transitionTimes[self.i%self.n] = self.startTime - time.time()
-            self.look_for_1 = False
-            self.i += 1
-        elif not self.look_for_1 and c1_val == 1:
-            self.transitionTimes[self.i%self.n] = self.startTime - time.time()
-            self.look_for_1 = True
-            self.i += 1
+        if self.look_for_1A and c1a_val == 0:
+            self.transitionTimesA[self.a%self.n] = self.startTime - time.time()
+            self.look_for_1A = False
+            self.a += 1
+        elif not self.look_for_1A and c1a_val == 1:
+            self.transitionTimesA[self.a%self.n] = self.startTime - time.time()
+            self.look_for_1A = True
+            self.a += 1
+
+        if self.look_for_1B and c1b_val == 0:
+            self.transitionTimesB[self.b%self.n] = self.startTime - time.time()
+            self.look_for_1B = False
+            self.b += 1
+        elif not self.look_for_1B and c1b_val == 1:
+            self.transitionTimesB[self.b%self.n] = self.startTime - time.time()
+            self.look_for_1B = True
+            self.b += 1
 
         # calculate speeds
-        if self.i > self.n:
-            speed = 1/(self.transitionTimes[(self.i-1)%self.n] - self.transitionTimes[(self.i-self.n)%self.n])
+        if self.a > self.n:
+            self.speedA = 1/(self.transitionTimesA[(self.a-1)%self.n] - self.transitionTimesA[(self.a-self.n)%self.n])
         else:
-            speed = 0
+            self.speedA = 0
+
+        if self.b > self.n:
+            self.speedB = 1/(self.transitionTimesB[(self.b-1)%self.n] - self.transitionTimesB[(self.b-self.n)%self.n])
+        else:
+            self.speedB = 0
+
+        # adjust speed with PID
+
+        self.prev_error = self.error
+        self.error = self.speedA - self.speedB
+        self.sum_error += self.error
+
+        # pwmB = self.Kp * self.error + self.Ki * self.sum_error + self.Kd * (self.error - self.prev_error) + self.duty + 10
 
         self.get_logger().info('Speed: %f' % speed)
 
